0e5bc8597017233c6d71a453bd64e0ac02491549
Thomas-Wayne <lvrsmedic@yahoo.com>
2018-03-11 20:48:58 -0400

    .

---
+++ assignments/weathersite/scripts/scripts.js
@@ -0,0 +1,22 @@
+//Hamburger menu
+
+function toggleNavMenu(){
+    
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+
+//Current date
+
+var DaysofWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var MonthsinaYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var d = new Date();
+var dayName = DaysofWeek[d.getDay()];
+var day = d.getDate();
+var monthName = MonthsinaYear[d.getMonth()];
+var year = d.getFullYear();
+
+document.getElementById("currentdate").innerHTML = dayName + ", " + monthName + " " + day +  ", " + year;
+
+
