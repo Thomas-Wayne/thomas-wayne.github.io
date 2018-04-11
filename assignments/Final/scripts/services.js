cb2c292faa942cf164581987f5d0ec5e2c0f05cc
Wayne Thomas <wayne@baylands.local>
2018-04-11 15:28:36 -0400

    updates

--- assignments/Final/scripts/services.js
+++ assignments/Final/scripts/services.js
@@ -1,38 +1,20 @@
              
-/*        var request = new XMLHttpRequest();
+      var request = new XMLHttpRequest();
         request.open('GET', 'https://thomas-wayne.github.io/assignments/Final/data/services.json', true);
         request.responseType = 'json';
         request.send();
         
-        /*request.onload = function() {
+        request.onload = function() {
             var services = request.response;
             console.log(services);
-        }//end of request*/
+        }//end of request
 
 
-function onLoad(){
-			var url='https://thomas-wayne.github.io/assignments/Final/data/services.json';
-			$.getJSON(url, function(json){
-				var table = $('<table>');
-				table.attr('border','1');
-				var tr = $('<tr>');
-				var td = $('<td>');
-				td.html("Service");
-				tr.append(td);
-				td = $('<td>');
-				td.html('Price');
-				tr.append(td);
-				table.append(tr);
-				for( var i=0; i<json.length;i++){
-					var tr = $('<tr>');
-					var td = $('<td>');
-					td.html(json[i].service);
-					tr.append(td);
-					td = $('<td>');
-					td.html(json[i].price);
-					tr.append(td);
-					table.append(tr);
-				}
-				$('body').append(table);
-			});
-		}
+        function services(arr) {
+            var out = "";
+            var i;
+            for(i = 0; i<arr.length; i++) {
+                out += "Service: "+ arr[i].service + "Price: " + arr[i].price + '<br>';
+        }
+            document.getElementById("id01").innerHTML = out;
+}
