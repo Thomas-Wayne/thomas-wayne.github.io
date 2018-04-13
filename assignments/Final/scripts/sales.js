d570fd3aaf821d42fe6d14c071cc05f5cb9d178e
Wayne Thomas <wayne@baylands.local>
2018-04-13 11:42:10 -0400

    .

--- assignments/Final/scripts/sales.js
+++ assignments/Final/scripts/sales.js
@@ -2,4 +2,24 @@ $(document).ready(function(){
     $("#flip").click(function(){
         $("#panel").toggle(1000);
     });
+
+    $("#flip1").click(function(){
+        $("#panel1").toggle(1000);
+    });
+
+    $("#flip2").click(function(){
+        $("#panel2").toggle(1000);
+    });
+
+    $("#flip3").click(function(){
+        $("#panel3").toggle(1000);
+    });
+
+    $("#flip4").click(function(){
+        $("#panel4").toggle(1000);
+    });
+
+    $("#flip5").click(function(){
+        $("#panel5").toggle(1000);
+    });
 });
