350af262aef918b34db91ed42f8af0a662b4e70e
Wayne Thomas <wayne@baylands.local>
2018-04-13 10:39:24 -0400

    .

--- assignments/Final/scripts/sales.js
+++ assignments/Final/scripts/sales.js
@@ -1,39 +1,5 @@
-$(document).ready(function($){
-                  
-var imageBox = $('.slider ul');
-var imageWidth = $('.slider ul li').first().children('img').width();
-var imageQuantity = $('slider ul').children('li').length;
-var currentImage = 1;
-
-imageBox.css('width', imageWidth*imageQuantity);
-
-$('.nav button').on('click', function(){
-    var whichButton = $(this).data('nav');
-    
-    if(whichButton === 'next'){
-        if(currentImage === imageQuantity){
-            currentImage = 1;
-            transition(currentImage, imageWidth);
-        }else{
-            currentImage++;
-            transition(currentImage, imageWidth);
-        }
-    }else if(whichButton === 'prev'){
-        if(currentImage === 1){
-            currentImage = imageQuantity;
-            transition(currentImage, imageWidth);
-        }else{
-            currentImage--;
-            transition(currentImage, imageWidth);
-        }
-    }
-});
-
-function transition(currentImageInput, ImageWidthInput){
-    var pxValue = -(currentImageInput -0) * ImageWidthInput
-    
-    imageBox.animate({
-        'left': pxValue
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").toggle(1000);
     });
-}
 });
